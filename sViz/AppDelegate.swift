9f8a450eff39a63f40fca6952263808aafaecbd6
mcmhav <mcmhav@gmail.com>
2017-08-16 13:52:28 +0200

    Initial Commit

---
+++ sViz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  sViz
+//
+//  Created by Martin Havig on 16/08/2017.
+//  Copyright © 2017 Cake. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ sViz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  sViz
+//
+//  Created by Martin Havig on 16/08/2017.
+//  Copyright © 2017 Cake. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

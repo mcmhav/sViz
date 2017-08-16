a6f89cd0f2d93c0983a7afeb70c21577b35a0ce7
mcmhav <mcmhav@gmail.com>
2017-08-16 16:24:14 +0200

    initial thing

--- sViz/AppDelegate.swift
+++ sViz/AppDelegate.swift
@@ -10,17 +10,26 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    let statusItem = NSStatusBar.systemStatusBar().statusItemWithLength(-2)
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        if let button = statusItem.button {
+            button.image = NSImage(named: "StatusBarButtonImage")
+            button.action = Selector("printQuote:")
+        }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func printQuote(sender: AnyObject) {
+        let quoteText = "Never put off until tomorrow what you can do the day after tomorrow."
+        let quoteAuthor = "Mark Twain"
+        
+        println("\(quoteText) — \(quoteAuthor)")
+    }
 }
 
